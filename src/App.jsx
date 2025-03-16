c547a86bc998eb4278ddfe8607eea934d261ac3d
Noah K (2ofeveryanimal everywhere) <41722860+2of@users.noreply.github.com>
2025-03-16 15:12:15 +1300

    misc gh fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { GlobalProvider, useGlobalContext } from "./contexts/GlobalContext"; // Import GlobalProvider and useGlobalContext
 import routes from "./routes/routes"; // Import routes
 import MainLayout from "./layouts/MainLayout/MainLayout"; // Import MainLayout
@@ -22,31 +22,28 @@ function App() {
 }
 
 const AppContent = () => {
-  const { basename } = useGlobalContext(); // Access basename from GlobalContext
-
   return (
-    // <Router basename={basename}> {/* Use the basename from context */}
-    //   <Routes>
-    //     {/* Routes that use MainLayout */}
-    //     <Route element={<MainLayout />}>
-    //       {/* Dynamically render routes from the routes array */}
-    //       {routes.map((route) => (
-    //         <Route 
-    //           key={route.path} 
-    //           path={route.path} 
-    //           element={route.element} 
-    //         />
-    //       ))}
-    //     </Route>
+    <Router> {/* No need for basename with HashRouter */}
+      <Routes>
+        {/* Routes that use MainLayout */}
+        <Route element={<MainLayout />}>
+          {/* Dynamically render routes from the routes array */}
+          {routes.map((route) => (
+            <Route 
+              key={route.path} 
+              path={route.path} 
+              element={route.element} 
+            />
+          ))}
+        </Route>
 
-    //     {/* 404 Route (doesn't use MainLayout) */}
-    //     {/* <Route path="/404" element={<NotFoundPage />} /> */}
+        {/* 404 Route (doesn't use MainLayout) */}
+        <Route path="/404" element={<NotFoundPage />} />
 
-    //     {/* If the route doesn't match any, redirect to the /404 route */}
-    //     {/* <Route path="*" element={<Navigate to="/404" />} /> */}
-    //   </Routes>
-    // </Router>
-    <h1>test</h1>
+        {/* If the route doesn't match any, redirect to the /404 route */}
+        <Route path="*" element={<Navigate to="/404" />} />
+      </Routes>
+    </Router>
   );
 }
 
